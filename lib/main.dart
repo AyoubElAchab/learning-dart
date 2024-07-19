1a1a2ce85bc70d3a0ba11cc6c3b0a029bcd1745d
ayoub el achab <elachabayoubdev@gmail.com>
2024-07-20 00:46:51 +0100

    how to use assets images

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,31 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('my first app1 2 3'),
-      centerTitle: true,
-      backgroundColor : Colors.red[600],
-
-    ),
-    body: Center(
-      child: Text(
-        'hello world',
-        style : TextStyle(
-          fontSize : 20.0,
-          fontWeight : FontWeight.bold,
-          letterSpacing : 2.0,
-          color : Colors.grey[600],
-          fontFamily : 'IndieFlower',
-        ),
-      ),
-    ),
-    floatingActionButton: FloatingActionButton(
-      onPressed: () {
-        // Define your onPressed action here
-      },
-      child: Text('click'),// Use an Icon widget here
-      backgroundColor : Colors.red[600],
-    ),
-  ),
+  home: Home(),
 ));
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('my first app1 2 3'),
+        centerTitle: true,
+        backgroundColor : Colors.red[600],
+      ),
+      body: Center(
+          child: Image.asset('assets/img-2.jpg'),
+          // child: Text('ayoub'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+        // Define your onPressed action here
+        },
+        child: Text('click '),// Use an Icon widget here
+        backgroundColor : Colors.red[600],
+      ),
+    );
+  }
+}
